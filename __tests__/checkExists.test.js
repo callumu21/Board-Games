a04fa79986eb4a58ac6c7d69923c758358c35a12
callumu21 <cmu_92@hotmail.co.uk>
2022-08-03 09:27:16 +0100

    Change wording of checkExists tests

--- __tests__/checkExists.test.js
+++ __tests__/checkExists.test.js
@@ -1,12 +1,12 @@
 const { checkExists } = require("../utils/checkExists");
 
 describe("checkExists function", () => {
-  test("does not return a promise if item exists in the correct table and column", () => {
+  test("does not return an error if item exists in the correct table and column", () => {
     return checkExists("reviews", "review_id", 1).then((output) => {
-      expect(output).not.toBeInstanceOf(Promise);
+      expect(output).toBe(undefined);
     });
   });
-  test("returns a rejected promise if item is valid but does not exist in the correct table and column", () => {
+  test("returns a rejected promise with an error if item is valid but does not exist in the correct table and column", () => {
     return checkExists("reviews", "review_id", 10000).catch((error) => {
       expect(error).toEqual({
         status: 404,
